86e1994857844f438e0678f0e1e768c206ff2303
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2022-11-04 03:20:52 -0300

    OK: Agrega DataModel Course

---
+++ UniversityOB.API/Models/DataModels/Course.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityOB.API.Models.DataModels
+{
+    public enum CourseLevel
+    {
+        Basic,
+        Medium,
+        Advanced
+    }
+
+    public class Course : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        public string LongDescription { get; set; } = string.Empty;
+
+        public string TargetAudience { get; set; } = string.Empty;
+
+        public string Objectives { get; set; } = string.Empty;
+
+        public string Requisites { get; set; } = string.Empty;
+
+        public CourseLevel Level { get; set; } = CourseLevel.Basic;
+    }
+}
